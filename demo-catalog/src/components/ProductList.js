5982f02d294ec30b89de3d7c5581a840538c8430
MansuraAkhter <mansura3330@gmail.com>
2023-01-09 00:31:28 +0600

    few fixes

--- demo-catalog/src/components/ProductList.js
+++ demo-catalog/src/components/ProductList.js
@@ -3,11 +3,14 @@ const ProductList = ({ products, addToCart }) => {
     <div className="catalog-details">
       {products.map((product) => (
         <div className="inside-details" key={product.id}>
-          <img src={product.image_url} alt="" width="300" />
-          <h2>Vender code: {product.code}</h2>
+          <img src={product.image} alt="" width="300" />
+          <h2>Vender code: {product.id}</h2>
           <p>{product.title}</p>
+          <p>{product.description}</p>
+          <br />
           <h3>Price:</h3>
           <h5>{product.price}$</h5>
+
           <button
             onClick={() => {
               addToCart(product)
